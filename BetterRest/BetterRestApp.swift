9388bef475ed0d7f77601a10c500e2a1448821ff
LuiArm <Luisarmendariz24@icloud.com>
2023-04-13 20:31:06 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by luis armendariz on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
