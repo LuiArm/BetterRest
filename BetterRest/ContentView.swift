2d0ea63736cf927b6bd7e15ad252123075e8c361
LuiArm <Luisarmendariz24@icloud.com>
2023-04-20 16:22:38 -0600

    Change UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -35,52 +36,75 @@ struct ContentView: View {
                         .labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: 0){
+                Section{
                     Text("Desires amount of sleep")
                         .font(.headline)
                     
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
-                                
-                VStack(alignment: .leading, spacing: 0){
+                
+                Section{
                     Text("Daily coffee intake")
                         .font(.headline)
                     
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", selection: $coffeeAmount){
+                        ForEach(0..<21){
+                            Text("\($0) cups")
+                        }
+                    }
                 }
-            }
-            .navigationTitle("BetterSleep")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert){
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
+                
+                Section{
+                    Text("Recommended Bedtime")
+                        .font(.headline)
+                    Text("\(calculateBedTime)")
+                        .font(.largeTitle)
+                }
+                .navigationTitle("BetterSleep")
             }
         }
-    }
-    
-    func calculateBedTime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-        }catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, something went wrong!"
+        
+        var calculateBedTime: String {
+            var bedTime = "Please set all input values"
+            do {
+                let config = MLModelConfiguration()
+                let model = try SleepCalculator(configuration: config)
+                
+                let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                let hour = (components.hour ?? 0) * 60 * 60
+                let minute = (components.minute ?? 0) * 60
+                
+                
+                let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+                let sleepTime = wakeUp - prediction.actualSleep
+                
+                
+                let formatter = DateFormatter()
+                formatter.timeStyle = .short
+                bedTime = formatter.string(from: sleepTime)
+                //            alertTitle = "Your ideal bedtime is..."
+                //            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            }catch {
+                alertTitle = "Error"
+                alertMessage = "Sorry, something went wrong!"
+                showingAlert = true
+            }
+            return bedTime
         }
-        showingAlert = true
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
